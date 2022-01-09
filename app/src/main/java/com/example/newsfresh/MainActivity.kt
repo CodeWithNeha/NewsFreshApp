9517a7f5669cedb48d51a06bd2d5ba196216d22b
Neha <codewithneha2020@gmail.com>
2022-01-09 22:01:55 +0530

    Day 2

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -5,26 +5,51 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
+    private lateinit var mAdapter: NewsListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<RecyclerView>(R.id.recyclerView).layoutManager = LinearLayoutManager(this)
-        val items = fetchData()
-        val adapter = NewsListAdapter(items,this)
-        findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
+        fetchData()
+        val adapter = NewsListAdapter(this)
+        findViewById<RecyclerView>(R.id.recyclerView).adapter = mAdapter
 
     }
-    private fun fetchData(): ArrayList<String>{
-        val list = ArrayList<String>()
-        for (i in 0 until 100){
-            list.add("Item $i")
-        }
-        return list
+    private fun fetchData(){
+        val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=2480341fde894b9388e6f9959ac8b7be"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+                mAdapter.updateNews(newsArray)
+            },
+            Response.ErrorListener {
+
+            },
+
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this ,"clicked item is $item", Toast.LENGTH_LONG).show()
+    override fun onItemClicked(item: News) {
+
     }
 }
--- app/src/main/java/com/example/newsfresh/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsfresh/NewsListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.newsfresh
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.util.ArrayList
 
-class NewsListAdapter(private val items: ArrayList<String>, private val listener: MainActivity): RecyclerView.Adapter<NewsviewHolder>() {
+class NewsListAdapter( private val listener: MainActivity): RecyclerView.Adapter<NewsviewHolder>() {
+    private val items: ArrayList<News> = ArrayList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsviewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
         val viewHolder = NewsviewHolder(view)
@@ -20,11 +22,19 @@ class NewsListAdapter(private val items: ArrayList<String>, private val listener
 
     override fun onBindViewHolder(holder: NewsviewHolder, position: Int) {
         val currentItem = items[position]
-        holder.titleView.text = currentItem
+        holder.titleView.text = currentItem.title
     }
 
     override fun getItemCount(): Int {
         return items.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateNews(updatedNews: ArrayList<News>){
+        items.clear()
+        items.addAll(updatedNews)
+
+        notifyDataSetChanged()
     }
 
 }
@@ -33,5 +43,5 @@ class NewsviewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 }
 
 interface NewsItemClicked{
-    fun onItemClicked(item: String)
+    fun onItemClicked(item: News)
 }
